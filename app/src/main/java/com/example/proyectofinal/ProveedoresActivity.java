d0c4d6aaf6c4139741a6008b7268cc156128fe16
Canelita <ing69chipmunks@hotmail.com>
2023-11-01 18:30:43 -0300

    Vamos probando carajp (ㅅ´ ˘ `)

---
+++ app/src/main/java/com/example/proyectofinal/ProveedoresActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProveedoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proveedores);
+    }
+}
