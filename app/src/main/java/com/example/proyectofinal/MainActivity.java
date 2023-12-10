81af80eb2902cae6ac820449acf32a4421c79948
Canelita <ing69chipmunks@hotmail.com>
2023-12-10 00:48:15 -0300

    EAOOO EAO EAOOO (ㅅ´ ˘ `)

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton agregarinsumo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        agregarinsumo = findViewById(R.id.fab);
+
+        agregarinsumo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AgregarInsumosActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
